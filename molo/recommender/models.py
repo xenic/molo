db87dbbda8fefd60c70aff303a9321ebf9bd050f
Sam Boyarsky <sam.boyarsky@Sams-MacBook-Pro.local>
2016-08-22 14:06:06 -0400

    added additional data files, initial migrations

--- molo/recommender/models.py
+++ molo/recommender/models.py
@@ -1,32 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Genre(models.Model):
+class StandardRepr(models.Model):
+    def __repr__(self):
+        vals = []
+        for f in self.__dict__:
+            if not f.startswith('_'):
+                vals.append(f + '=' + self.__dict__[f].__repr__())
+        return '{}({})'.format(self.__class__.__name__, ', '.join(vals))
+
+    class Meta:
+        abstract = True
+
+
+class Genre(StandardRepr):
     name = models.CharField(max_length=32)
 
 
-class Occupation(models.Model):
+class Occupation(StandardRepr):
     name = models.CharField(max_length=32)
 
 
-class AgeRange(models.Model):
+class AgeRange(StandardRepr):
     value = models.IntegerField()
     description = models.CharField(max_length=16)
 
 
-class Movie(models.Model):
+class Movie(StandardRepr):
     title = models.CharField(max_length=256)
     year = models.IntegerField(null=True)
-    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genre)
 
-class Rater(models.Model):
+
+class Rater(StandardRepr):
     gender = models.CharField(max_length=8)
     age_range = models.ForeignKey(AgeRange, on_delete=models.CASCADE)
     occupation = models.ForeignKey(Occupation, on_delete=models.CASCADE)
-    zipcode = models.IntegerField(null=True)
+    zipcode = models.CharField(max_length=10)
+    user = models.OneToOneField(User)
 
-class Rating(models.Model):
+
+class Rating(StandardRepr):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     rater = models.ForeignKey(Rater, on_delete=models.CASCADE)
     rating = models.IntegerField()
-    timestamp = models.DateTimeField(auto_now=False)
+    rate_time = models.DateTimeField(auto_now=False)
